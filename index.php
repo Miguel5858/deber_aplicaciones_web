2e6d4a242f8a79b62af6d48af9a2bbc306a2d88f
Miguel5858 <jmti4884@gmail.com>
2023-07-16 20:46:09 -0500

    cambio tamanio boton

--- index.php
+++ index.php
@@ -30,12 +30,46 @@ if(!isset($_SESSION['usuario'])){
 
  <h1>Hola Mundo</h1>
 
- <a class="btn btn-danger" href="controladores/cerrar.php">Cerrar Sesion</a>
+ <a class="btn btn-danger btn-sm" href="controladores/cerrar.php">Cerrar Sesion</a>
 
- <div class="mt-4 p-5 bg-primary text-white rounded">
-  <h1>Bienvenido</h1>
-  <p>Esta es solo una prueba</p>
-</div>
+
+ <div class="card mt-4">
+  <div class="card-header">
+    Lista de Registros
+    <a class="btn btn-primary btn-sm" href="#">Nuevo Registro</a>
+  </div>
+  <div class="card-body">
+ 
+  <div class="table-responsive-sm">
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Nombres</th>
+          <th scope="col">Apellidos</th>
+          <th scope="col">Telefono</th>
+          <th scope="col">Direccion</th>
+          <th scope="col">Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Miguel Jose</td>
+          <td>Intriago tobar</td>
+          <td>0996193351</td>
+          <td>Quito, Cotocollao</td>
+          <td>
+          <a class="btn btn-warning btn-sm" href="#">Editar</a> | <a class="btn btn-danger btn-sm" href="#">Eliminar</a>            
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  
+  </div>
+ </div>
+
+
+ 
 
 </div>
 

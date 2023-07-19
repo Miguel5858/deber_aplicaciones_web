b76109cd6e70b5f46e40b1dd8ad70bb331b7ed23
Miguel5858 <jmti4884@gmail.com>
2023-07-18 19:32:16 -0500

    listo

--- controladores/conexionBaseDatos.php
+++ controladores/conexionBaseDatos.php
@@ -7,6 +7,10 @@ $usuario="root";
 $contrasenia="";
 
 try{
+
+//PDO es una forma de conectarse y trabajar con bases de datos utilizando una 
+//interfaz común y abstracción para diferentes tipos de bases de datos en PHP.
+
 $conexion=new PDO("mysql:host=$servidor;dbname=$baseDeDatos",$usuario,$contrasenia);
  //echo "Conexión realizada...";
 
